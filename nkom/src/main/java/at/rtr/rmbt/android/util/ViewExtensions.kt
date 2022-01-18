d38c49e2c22b52aa35eab5ddc5b18b612c1d49cd
michal cadrik <michal.cadrik@martes-specure.com>
2022-01-18 10:42:34 +0100

    fixing code style

--- nkom/src/main/java/at/rtr/rmbt/android/util/ViewExtensions.kt
+++ nkom/src/main/java/at/rtr/rmbt/android/util/ViewExtensions.kt
@@ -84,12 +84,12 @@ fun Paint.calcTextHeight(demoText: String): Float {
  * This function is used for format value up-to 2 decimal
  */
 fun Float.format(): String {
-   return when {
-       this < 1.0 -> DecimalFormat("###.###").format(this)
-       this < 10.0 -> DecimalFormat("###.##").format(this)
-       this < 100.0 -> DecimalFormat("###.#").format(this)
-       else -> DecimalFormat("###").format(this)
-   }
+    return when {
+        this < 1.0 -> DecimalFormat("###.###").format(this)
+        this < 10.0 -> DecimalFormat("###.##").format(this)
+        this < 100.0 -> DecimalFormat("###.#").format(this)
+        else -> DecimalFormat("###").format(this)
+    }
 }
 
 /**
